0c0970998821fe3ed6fdb5e1593dc5ffe7944b29
KennardLopez <kennardmamucodlopez@gmail.com>
2024-03-12 16:07:16 +0800

    BINGO

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
  console.log("Hello World.");
 
 function generateRamdomNumber() {
-    let randomNum = Math.floor(Math.random() * 10) + 1;
+    let randomNum = Math.floor(Math.random() * 15) + 1;
     if(randomNum >= 5){
         console.log(`Random Number is > = to 5`);
     } else {
